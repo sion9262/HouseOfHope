c2ac76f861a0f661c6eaaeb7ee38a6af1ae15904
sion <parksion9262@gmail.com>
2020-11-16 18:28:50 +0900

    start backend add

--- backend/main.py
+++ backend/main.py
@@ -54,3 +54,7 @@ def get_guest_info(user: GuestClass.UserModel):
 def update_guest(update: GuestClass.UpdateModel):
     datas = GuestClass.update_guest(update)
     return datas
+
+
+if __name__=="__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port="3000", log_level='info', access_log=False)
